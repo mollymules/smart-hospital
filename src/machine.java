954ac29ffc19914d6597f6e81f12e58002c64270
Kevnewman89 <Kevnewman89@gmail.com>
2011-10-14 08:43:29 +0000

    Filled out both the machine and BP_machaine classes, started into the zeroConf stuff.....

--- src/machine.java
+++ src/machine.java
@@ -1,13 +1,23 @@
 //Code for all the Machines to implement from
 public interface machine {
 
-	public void getPatient_ID();
+	//All Method in an interface are public
+	int P_ID = 0;
+	
+	void getPatient_ID();
+	
+	boolean has_Patient();
+	
+	//Completes the specific job for each machine
+	void completeTask();
+	
+	//cross the task off the list?
+	void checkList();
 	
 	
+	void getResults();
 	
 	
-	
-	
-	
-	
+	void toServer();
+
 }
