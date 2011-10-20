98bbfbb153815c528e395c539516dad75324d3cd
Kevnewman89 <Kevnewman89@gmail.com>
2011-10-20 15:39:22 +0000

    Have the BP_machine working, outputting results from a range between 70/50  to 120/100. Run it using the Test main

---
+++ src/testMain.java
@@ -0,0 +1,15 @@
+
+public class testMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		BP_machine BP = new BP_machine(101);
+		BP.completeTask();
+		System.out.print(BP.getResults());
+
+	}
+
+}
