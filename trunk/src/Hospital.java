6e5e39a06bc9a05789a7cd23c7dd401e9362c23b
maryseery20 <maryseery20@5c981e56-9f92-4549-b090-4880e0a73a9a>
2011-11-29 20:09:03 +0000

    Committing patient

---
+++ trunk/src/Hospital.java
@@ -0,0 +1,46 @@
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.util.Hashtable;
+
+
+public class Hospital {
+	BP_machine bp1;
+	Hashtable<Integer, String> allTests;
+	
+	public Hospital(int numPatients){
+		allTests = new Hashtable<Integer, String>();
+		allTests.put(0, "BloodPressure");
+		allTests.put(1, "XRay");
+		allTests.put(2, "CatScan");
+		allTests.put(3, "Temp");
+		for(int i = 0; i< numPatients; i++){
+			createPatient(i);
+		}
+		/*try {
+			bp1 = new BP_machine("Ward 1","230.0.0.2", 4444);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}*/
+	}
+	
+	public void createPatient(int i){
+		String ward = "Ward "+((int)(Math.random()*3)+1);
+		Patient m = new Patient(i, ward);
+		m.addTest(addTest());
+		Thread k = new Thread(m);
+		k.start();
+		System.out.println("Patient "+i+" is in "+ ward);
+	}
+	
+	public String addTest(){
+		String test = allTests.get((int)Math.random()*allTests.size());
+		return test;
+	}
+	
+	public static void main(String[] args){
+		Hospital hos = new Hospital(10);
+	}
+
+}
